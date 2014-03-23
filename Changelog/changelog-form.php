bf2f76301d31aa160e8c75d73b388d0fb9582c6c
DarkStoorM <b.sieprawski@op.pl>
2014-03-23 13:50:13 +0100

    Add customisable changelog form in different file.

---
+++ Changelog/changelog-form.php
@@ -0,0 +1,9 @@
+<form method="POST">
+	<select>
+		<option value="a">Add</option>
+		<option value="f">Fix</option>
+		<option value="r">Remove</option>
+	</select>
+	<input type="text" name="description">
+	<input type="submit" name="submit_change">
+</form>
