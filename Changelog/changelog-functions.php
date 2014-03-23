0f4d025c0a113d46814d0bdf8bfe5757edb494e3
DarkStoorM <b.sieprawski@op.pl>
2014-03-23 13:52:04 +0100

    Clean up and load form from a file.

--- Changelog/changelog-functions.php
+++ Changelog/changelog-functions.php
@@ -1,5 +1,17 @@
 <?php
 	class changelog
 	{
-	
+		public $description_length = null;
+		
+		public function __construct($settings)
+		{
+			global $settings;
+			
+			$this->description_length=$settings["description_length"];
+		}
+		
+		public function form()
+		{
+			include("changelog-form.php");
+		}
 	}
