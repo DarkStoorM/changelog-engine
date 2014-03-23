bd54aa581a7e936c669785f7be599d53ba1c962b
DarkStoorM <b.sieprawski@op.pl>
2014-03-23 16:07:47 +0100

    Save database template just in case.

---
+++ Changelog/changelog-db.sql
@@ -0,0 +1,12 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+CREATE DATABASE IF NOT EXISTS `changelog` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `changelog`;
+
+CREATE TABLE IF NOT EXISTS `changes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` set('a','f','r') NOT NULL DEFAULT 'a',
+  `description` varchar(100) NOT NULL,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=14 ;
